d32ec240df1fbf38f1e6a0ee8014ced30fb9deb6
fardin1999 <shihiyev@gmail.com>
2015-03-10 00:01:25 +0400

    calculator.py
    A very basic calculator program with addition, subtraction, multiplication, division and modulo operations written in python 2.

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+__author__ = 'fardin'
+
+"""A very basic calculator program with \
+ addition, subtraction, multiplication, division and modulo operations written in python 2."""
+
+while True:
+    option = int(raw_input(
+        'Welcome to calculator program.\n1)Add\n2)Subtract\n3)Multiply\n4)Divide\n5)Modulo\nChoose\
+         one of the options above: '))  # menu
+    num1 = float(raw_input('Enter first number: '))  # first number
+    num2 = float(raw_input('Enter second number: '))  # second number
+    if option == 1:
+        print num1, '+', num2, '=', num1 + num2  # addition
+    elif option == 2:
+        print num1, '-', num2, '=', num1 - num2  # subtraction
+    elif option == 3:
+        print num1, '*', num2, '=', num1 * num2  # multiplication
+    elif option == 4:
+        print num1, '/', num2, '=', num1 / num2  # division
+    elif option == 5:
+        print num1, '%', num2, '=', num1 % num2  # modulo
+    else:
+        print 'Invalid input'
