a2210808355f5d6f8ee56c0a4c1b767f04df2c78
fardin1999 <shihiyev@gmail.com>
2015-03-10 00:36:17 +0400

    Find number of letters in a sentence.
    Find number of letters in a sentence.

--- letter.py
+++ letter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# coding utf-8
+# coding:utf-8
 __author__ = 'fardin'
 """A simple program to find number of letters (may be numbers when string-converted) in a sentence"""
 while True:
