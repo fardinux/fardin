fb6b775794fcec6a8b771fbef1a3df53e338dcff
fardin1999 <shihiyev@gmail.com>
2015-03-21 22:03:49 +0400

    A simple program that "tells" you current date and time
    A simple program that "tells" you current date and time. Written in Python2.

---
+++ datetime.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# A simple programs which "tells" current date and time
+# Written in Python2
+
+from datetime import datetime
+now=datetime.now()
+
+print "Today is: %s/%s/%s" %(now.day, now.month, now.year)
+print "Current time is: %s:%s:%s" %(now.hour, now.minute, now.second)
