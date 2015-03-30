382769eadc4169f3b2d649efad69595e0c5b8718
fardin1999 <shihiyev@gmail.com>
2015-03-30 23:10:57 +0500

    A simple number guess game.
    Just another number guess game yet. Written in Python 2x. Try to find the number that computer produced randomly!

---
+++ number_guess.py
@@ -0,0 +1,18 @@
+__author__ = 'fardin'
+from random import randint
+
+a = randint(1, 100)
+try:
+    while True:
+        x = int(raw_input('Enter a number from 1 to 100: '))
+        if x == a:
+            print 'You win!'
+            break
+        elif x > a:
+            print 'Try a smaller number!'
+        elif x < a:
+            print 'Try a bigger number!'
+        if x>100:
+            print 'Your number is too big!'
+except ValueError:
+    print 'Enter a number!'
