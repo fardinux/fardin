154409dba49a0d1d0718bb85fa343bc919c7a7c5
Fardin Shikhiyev <shihiyev@gmail.com>
2015-07-26 19:30:33 +0500

    **secret** formula of Coca-Cola in a python module!
    **secret** formula of Coca-Cola in a python module!

---
+++ coke.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+
+"""import this module by typing 'import coke' in yourt Python \
+interpreter and get the coke formula! """
+
+cola = 'The primary ingredients of Coca-Cola syrup include either high fructose corn syrup or\
+ sucrose derived from cane sugar, caramel color, caffeine, phosphoric acid, coca extract, lime\
+ extract, vanilla, and glycerin.High fructose corn syrup or sucrose are\
+ overwhelmingly the major added ingredients: one 600 ml bottle (≈20.29 U.S. fl. oz.) of Coca\
+ Cola contains the approximate equivalent of 15 teaspoons of sugar. However, contrary to\
+ what is implied by the "cola" name, Coca-Cola syrup does not contain any kola nut extract.\
+ Since no kola extracts are present in the recipe, the primary taste of Coca-Cola comes from\
+ vanilla and cinnamon with trace amounts of orange, lime and lemon and spices such as nutmeg.'
+ 
+print cola
